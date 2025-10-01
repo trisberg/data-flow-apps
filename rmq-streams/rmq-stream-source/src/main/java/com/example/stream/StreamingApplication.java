bd10fb66873083b303d4fda32831eaf9e354748b
trisberg <thomas.risberg@broadcom.com>
2025-10-01 12:03:12 -0400

    update config for rmq streams support

--- rmq-streams/rmq-stream-source/src/main/java/com/example/stream/StreamingApplication.java
+++ rmq-streams/rmq-stream-source/src/main/java/com/example/stream/StreamingApplication.java
@@ -2,6 +2,11 @@ package com.example.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.stream.config.ProducerMessageHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.amqp.outbound.RabbitStreamMessageHandler;
+import org.springframework.messaging.MessageHandler;
 
 @SpringBootApplication
 public class StreamingApplication {
@@ -10,4 +15,12 @@ public class StreamingApplication {
 		SpringApplication.run(StreamingApplication.class, args);
 	}
 
+	@Bean
+	@ConditionalOnProperty(
+        name = "spring.cloud.stream.rabbit.bindings.output.producer.producer-type",
+        havingValue = "stream-async"
+    )
+	ProducerMessageHandlerCustomizer<MessageHandler> rabbitStreamMessageHandlerCustomizer() {
+		return (handler, destinationName) -> ((RabbitStreamMessageHandler) handler).setHeadersMappedLast(true);
+	}
 }
